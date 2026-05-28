c6b1a0a6689026cc625fefedf983b585988a62b2
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-05-28 10:36:13 +0700

    CircularQueuesArray 13

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -77,6 +77,13 @@ public:
         int FRONT_Position = FRONT;
         int REAR_Position = REAR;
 
+        // cek apakah antrian kosong
+        if (FRONT_Position == -1)
+        {
+            cout << "Queue is empty\n";
+
+            return;
+        }
         
     }
 }
